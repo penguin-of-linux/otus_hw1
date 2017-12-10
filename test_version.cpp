27a321f462dfcc380c6e49e1fff4e47aa7e4d6a6
Khapov Kirill <penguin_of_linux@mail.ru>
2017-12-10 16:40:20 +0500

    Add } to the end of test_version file

--- test_version.cpp
+++ test_version.cpp
@@ -4,9 +4,11 @@
 
 #include "lib.h"
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test_version);
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() < 0);
+}
+
 }
