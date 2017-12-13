f2690575fdc4ca6c51e72a1d50de0908df872b3a
Khapov Kirill <penguin_of_linux@mail.ru>
2017-12-13 11:22:17 +0500

    Syntax error

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     std::cout << "Version " << version() << " was started" << std::endl;
-
+    std::cout << "Hello, world!" << std::endl;
+    
     return 0;
 }
